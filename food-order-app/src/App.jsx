e0b0bb123e0c44be2a048bfad9b161efdac3777f
cadolphe <christopher.adolphe@gmail.com>
2022-01-18 07:47:39 +0400

    Created project directory

---
+++ food-order-app/src/App.jsx
@@ -0,0 +1,12 @@
+import { FiSmile } from "react-icons/fi";
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1><FiSmile color="#202124" /> Happy Meals</h1>
+    </div>
+  );
+}
+
+export default App;

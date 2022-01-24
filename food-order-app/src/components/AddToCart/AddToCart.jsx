77baa3b825d72a8b74d53b77db9dfca27b53092a
cadolphe <christopher.adolphe@gmail.com>
2022-01-24 06:43:06 +0400

    Added validation to AddToCart component

--- food-order-app/src/components/AddToCart/AddToCart.jsx
+++ food-order-app/src/components/AddToCart/AddToCart.jsx
@@ -10,32 +10,49 @@ import styles from './AddToCart.module.css';
 
 function AddToCart({ meal }) {
   const { onAddItem } = useContext(AppContext);
-  const [ amount, setAmount ] = useState(1);
+  const [ cartData, setCartData ] = useState({ amount: 1, error: null });
 
   const handleAmountChange = (event) => {
-    setAmount(+event.target.value);
+    if (+event.target.value < 1) {
+      setCartData({ amount: +event.target.value, error: 'Amount cannot be less than 1' });
+      
+      return;
+    }
+
+    if (+event.target.value > 5) {
+      setCartData({ amount: +event.target.value, error: 'Amount cannot be greater than 5' });
+      
+      return;
+    }
+
+    setCartData({ amount: +event.target.value, error: null });
   };
 
   const handleAddToCart = (item) => {
+    console.log('handleAddToCart called..');
     const { id, title, price } = item;
     const mealItems = [];
 
-    for (let count = 0; count < amount; count++) {
+    for (let count = 0; count < cartData.amount; count++) {
       mealItems.push({ id, title, price });
     }
 
     onAddItem(mealItems);
+    setCartData({ amount: 1, error: null });
   };
 
   return (
     <div className={ styles['add-to-cart'] }>
       <div className={ styles['add-to-cart__control'] }>
         <label className={ styles['add-to-cart__label'] } htmlFor="amount">Amount:</label>
-        <input className={ styles['add-to-cart__input'] } id="amount" type="number" min="1" max="10" value={ amount } onChange={ handleAmountChange } />
+
+        <input className={ styles['add-to-cart__input'] } id="amount" type="number" min="1" max="5" value={ cartData.amount } onChange={ handleAmountChange } />
       </div>
 
+      <span className={ cartData.error !== null ? `${styles['add-to-cart__error']} ${styles['add-to-cart__error--visible']}` : styles['add-to-cart__error'] }>{ cartData.error }</span>
+
       <div className={ styles['add-to-cart__action'] }>
-        <Button onHandleClick={ () => handleAddToCart(meal) }><FiPlusCircle size="1.5em" /> Add</Button>
+        <Button onHandleClick={ () => handleAddToCart(meal) } isDisabled={ cartData.error !== null }><FiPlusCircle size="1.5em" /> Add</Button>
       </div>
     </div>
   );
--- food-order-app/src/components/Button/Button.jsx
+++ food-order-app/src/components/Button/Button.jsx
@@ -3,17 +3,18 @@ import PropTypes from 'prop-types';
 
 import styles from './Button.module.css';
 
-function Button({ children, type = 'button', className, onHandleClick }) {
+function Button({ children, type = 'button', className, isDisabled = false, onHandleClick }) {
   const classes = className ? `${styles.button} ${styles[className]}` : `${styles.button}`;
 
   return (
-    <button className={ classes } type={ type } onClick={ onHandleClick }>{ children }</button>
+    <button className={ classes } type={ type } onClick={ onHandleClick } disabled={ isDisabled }>{ children }</button>
   );
 }
 
 Button.propTypes = {
   type: PropTypes.string,
   className: PropTypes.string,
+  isDisabled: PropTypes.bool,
   onHandleClick: PropTypes.func.isRequired
 };
 

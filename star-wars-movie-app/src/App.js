84c2ff456da893527d2180fd7b4c649ae1cf02a9
cadolphe <christopher.adolphe@gmail.com>
2022-01-29 08:27:16 +0400

    Understanding how to handle http requests in react components

--- star-wars-movie-app/src/App.js
+++ star-wars-movie-app/src/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import config from './config.json';
 
+import AddMovie from './components/AddMovie';
 import MoviesList from './components/MoviesList';
+
 import './App.css';
 
 function App() {
@@ -22,8 +24,10 @@ function App() {
   // ];
 
   const [ movies, setMovies ] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(false);
+  const [ error, setError ] = useState(null);
 
-  const handleFetchMovies = async () => {
+  const handleFetchMovies = useCallback(async () => {
     // fetch(config.apiEndpoint)
     //   .then(response => response.json())
     //   .then(data => {
@@ -31,32 +35,117 @@ function App() {
     //     setMovies(data.results)
     //   })
     //   .catch(error => console.log(error));
+    let response;
 
     try {
-      const response = await fetch(config.apiEndpoint);
-      const { results: movies } = await response.json();
+      setIsLoading(true);
+      setError(null);
+      
+      response = await fetch(config.apiEndpoint);
+      
+      if (!response.ok) {
+        throw new Error('Something went wrong while fetching movies.');
+      }
 
-      const transformedMovies = movies.map(movie => ({
-        id: movie.episode_id,
-        title: movie.title,
-        releaseDate: movie.release_date,
-        openingText: movie.opening_crawl
+      const data = await response.json();
+
+      const transformedMovies = Object.keys(data).map(key => ({
+        id: key,
+        title: data[key].title,
+        releaseDate: data[key].releaseDate,
+        openingText: data[key].openingText
       }));
 
       setMovies(transformedMovies);
     } catch (error) {
       console.log(error);
+      handleHttpError(response.status);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  const handleAddMovie = async (movie) => {
+    console.log('handleAddMovie called...', movie);
+    let response;
+
+    try {
+      const response = await fetch(config.apiEndpoint, {
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      if (!response.ok) {
+        throw new Error('Something went wrong while adding new movie.')
+      }
+
+      const data = await response.json();
+      console.log('response data: ', data);
+    } catch (error) {
+      console.log(error);
+      handleHttpError(response.status);
     }
   };
+
+  const handleHttpError = (statusCode) => {
+    switch (statusCode) {
+      case 400:
+        setError('BAD REQUEST! The server could not process the request due to malformed request syntax.')
+        break;
+
+      case 401:
+        setError('UNAUTHORIZED! The request has not been applied because it lacks valid authentication credentials.')
+        break;
+
+      case 403:
+        setError('FORBIDDEN! The server could not authorize access requested resource.')
+        break;
+
+      case 404:
+        setError('NOT FOUND! The server could not find a current representation for the requested resource.')
+        break;
+    
+      default:
+        break;
+    }
+  };
+
+  const handleContent = () => {
+    if (isLoading && !movies.length) {
+      return (<p>Loading...</p>);
+    }
+
+    if (error) {
+      return (<p>{ error }</p>);
+    }
+
+    if (!isLoading && !movies.length && !error) {
+      return (<p>No movies loaded.</p>);
+    }
+
+    if (movies.length) {
+      return (<MoviesList movies={movies} />);
+    }
+  };
+
+  useEffect(() => {
+    handleFetchMovies();
+  }, [handleFetchMovies]);
 
   return (
     <React.Fragment>
       <section>
+        <AddMovie onAddMovie={ handleAddMovie } />
+      </section>
+
+      <section>
         <button onClick={ handleFetchMovies }>Fetch Movies</button>
       </section>
-      <section>
-        <MoviesList movies={ movies } />
-      </section>
+
+      <section>{ handleContent() }</section>
     </React.Fragment>
   );
 }

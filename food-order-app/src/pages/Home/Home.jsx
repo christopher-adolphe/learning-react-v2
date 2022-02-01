d3d466583d5dd4b6ec447bc28403f14be1ffbdfd
cadolphe <christopher.adolphe@gmail.com>
2022-02-01 18:44:34 +0400

    Add custom hook to fetch meal via http request

--- food-order-app/src/pages/Home/Home.jsx
+++ food-order-app/src/pages/Home/Home.jsx
@@ -1,21 +1,45 @@
 import React, { useState, useEffect } from 'react';
 
-import { getMeals } from '../../services/fakeMealService';
+import config from '../../config.json';
+import useHttp from '../../hooks/use-http';
 
 import { MealList } from '../../components';
 
 function Home() {
   const [ meals, setMeals ] = useState([]);
+  const { isLoading, error, sendRequest: fetchMeals } = useHttp();
 
   useEffect(() => {
-    setMeals(getMeals());
-  }, []);
+    const handleResponseData = (data) => {
+      const fetchedMeals = Object.keys(data).map(key => ({
+        id: key,
+        title: data[key].title,
+        description: data[key].description,
+        category: data[key].category,
+        price: data[key].price
+      }));
+
+      setMeals(fetchedMeals);
+    };
+
+    fetchMeals({ url: `${config.apiEndpoint}/meals.json` }, handleResponseData);
+  }, [fetchMeals]);
+
+  let content = (<MealList items={ meals } />);
+
+  if (isLoading) {
+    content = (<p className="status-message">Loading meals...</p>);
+  }
+
+  if (error) {
+    content = (<p className="status-message">{ error }</p>);
+  }
 
   return (
     <main>
       <h2>Most Popular Meals</h2>
 
-      <MealList items={ meals } />
+      { content }
     </main>
   );
 }

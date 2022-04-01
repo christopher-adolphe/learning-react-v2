c0f430a9da26c2f400c30051d41730288f0122d2
cadolphe <christopher.adolphe@gmail.com>
2022-04-01 07:47:28 +0400

    Understanding how to create a custom store with react hooks

---
+++ replace-redux-with-hooks/src/hooks-store/store.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+
+let globalState = {};
+let listeners = [];
+let actions = {};
+
+export const useStore = (shouldListen = true) => {
+  const setState = useState(globalState)[1];
+
+  const dispatch = (action, payload) => {
+    const newState = actions[action](globalState, payload);
+
+    globalState = { ...globalState, ...newState };
+
+    for (let listener of listeners) {
+      listener(globalState);
+    }
+  };
+
+  useEffect(() => {
+    if (shouldListen) {
+      listeners.push(setState);
+    }
+
+    return () => {
+      if (shouldListen) {
+        listeners = listeners.filter(listener => listener !== setState);
+      }
+    };
+  }, [setState, shouldListen]);
+
+  return [ globalState, dispatch ];
+};
+
+export const initStore = (userActions, initialState) => {
+  if (initialState) {
+    globalState = { ...globalState, ...initialState }
+  }
+
+  actions = { ...actions, ...userActions };
+};

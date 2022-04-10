5f04d4c62f3e82a867686e8f0a564d176a7b29a6
cadolphe <christopher.adolphe@gmail.com>
2022-04-10 09:19:28 +0400

    React hooks summary - custom hooks

--- react-hooks-summary/src/components/Ingredients/Ingredients.js
+++ react-hooks-summary/src/components/Ingredients/Ingredients.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect, useCallback, useRef, useReducer, useMemo } from 'react';
 
+import useHttp from '../../hooks/http';
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
-const URL = 'https://react-http-c7523-default-rtdb.firebaseio.com/ingredients.json';
+const URL = '';
 
 const INGREDIENTS_ACTIONS = {
   SET: 'SET',
@@ -31,37 +32,38 @@ const ingredientsReducer = (state, action) => {
   }
 };
 
-const HTTP_ACTIONS = {
-  SEND: 'SEND',
-  RESPONSE: 'RESPONSE',
-  ERROR: 'ERROR',
-  CLEAR: 'CLEAR'
-};
+// const HTTP_ACTIONS = {
+//   SEND: 'SEND',
+//   RESPONSE: 'RESPONSE',
+//   ERROR: 'ERROR',
+//   CLEAR: 'CLEAR'
+// };
 
-const httpStatusReducer = (state, action) => {
-  const { type, payload } = action;
+// const httpStatusReducer = (state, action) => {
+//   const { type, payload } = action;
 
-  switch (type) {
-    case HTTP_ACTIONS.SEND:
-      return { isLoading: true, error: null };
+//   switch (type) {
+//     case HTTP_ACTIONS.SEND:
+//       return { isLoading: true, error: null };
 
-    case HTTP_ACTIONS.RESPONSE:
-      return { ...state, isLoading: false };
+//     case HTTP_ACTIONS.RESPONSE:
+//       return { ...state, isLoading: false };
 
-    case HTTP_ACTIONS.ERROR:
-      return { isLoading: false, error: payload.error };
+//     case HTTP_ACTIONS.ERROR:
+//       return { isLoading: false, error: payload.error };
 
-    case HTTP_ACTIONS.CLEAR:
-      return { ...state, error: null };
+//     case HTTP_ACTIONS.CLEAR:
+//       return { ...state, error: null };
   
-    default:
-      return state;
-  }
-};
+//     default:
+//       return state;
+//   }
+// };
 
 function Ingredients() {
   const [ ingredientLists, dispatchIngredientLists ] = useReducer(ingredientsReducer, []);
-  const [ httpStatus, dispatchHttpStatus ] = useReducer(httpStatusReducer, { isLoading: false, error: null });
+  // const [ httpStatus, dispatchHttpStatus ] = useReducer(httpStatusReducer, { isLoading: false, error: null });
+  const [ isLoading, error, data, sendRequest, requestOption, identifier, clear ] = useHttp();
   // const [ ingredientLists, setIngredientLists ] = useState([]);
   const [ searchTerm, setSearchTerm ] = useState('');
   // const [ isLoading, setIsLoading ] = useState(false);
@@ -70,146 +72,156 @@ function Ingredients() {
   const searchTermRef = useRef(null);
 
   const addIngredientHandler = useCallback(async (ingredient) => {
-    try {
-      // setIsLoading(true);
-      dispatchHttpStatus({ type: HTTP_ACTIONS.SEND });
-      const response = await fetch(URL, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'Application/json'
-        },
-        body: JSON.stringify(ingredient)
-      });
+    // try {
+    //   // setIsLoading(true);
+    //   dispatchHttpStatus({ type: HTTP_ACTIONS.SEND });
+    //   const response = await fetch(URL, {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'Application/json'
+    //     },
+    //     body: JSON.stringify(ingredient)
+    //   });
 
-      if (response.ok && response.status === 200) {
-        const data = await response.json();
-        // const newIngredient = { ...ingredient, id: data.name };
+    //   if (response.ok && response.status === 200) {
+    //     const data = await response.json();
+    //     // const newIngredient = { ...ingredient, id: data.name };
 
-        // setIngredientLists((prevState) => [ newIngredient, ...prevState ]);
-        dispatchIngredientLists({
-          type: INGREDIENTS_ACTIONS.ADD,
-          payload: {
-            ingredient: { ...ingredient, id: data.name }
-          }
-        });
-      } else {
-        throw new Error('Something went wront while adding ingredient.');
-      }
-    } catch (error) {
-      // setError(error.message);
-      dispatchHttpStatus({
-        type: HTTP_ACTIONS.ERROR,
-        payload: { error: error.message }
-      });
-    } finally {
-      // setIsLoading(false);
-      dispatchHttpStatus({ type: HTTP_ACTIONS.RESPONSE });
-    }
-  }, []);
+    //     // setIngredientLists((prevState) => [ newIngredient, ...prevState ]);
+    //     dispatchIngredientLists({
+    //       type: INGREDIENTS_ACTIONS.ADD,
+    //       payload: {
+    //         ingredient: { ...ingredient, id: data.name }
+    //       }
+    //     });
+    //   } else {
+    //     throw new Error('Something went wront while adding ingredient.');
+    //   }
+    // } catch (error) {
+    //   // setError(error.message);
+    //   dispatchHttpStatus({
+    //     type: HTTP_ACTIONS.ERROR,
+    //     payload: { error: error.message }
+    //   });
+    // } finally {
+    //   // setIsLoading(false);
+    //   dispatchHttpStatus({ type: HTTP_ACTIONS.RESPONSE });
+    // }
+
+    sendRequest(URL, 'POST', JSON.stringify(ingredient), ingredient, 'ADD_INGREDIENT');
+  }, [sendRequest]);
 
   const removeIngredientHandler = useCallback(async (id) => {
-    try {
-      // setIsLoading(true);
-      dispatchHttpStatus({ type: HTTP_ACTIONS.SEND });
-      const response = await fetch(`https://react-http-c7523-default-rtdb.firebaseio.com/ingredients/${id}.json`, {
-        method: 'DELETE'
-      });
+    // try {
+    //   // setIsLoading(true);
+    //   dispatchHttpStatus({ type: HTTP_ACTIONS.SEND });
+    //   const response = await fetch(``, {
+    //     method: 'DELETE'
+    //   });
 
-      if (response.ok && response.status === 200) {
-        // setIngredientLists((prevState) => prevState.filter(ingredient => ingredient.id !== id));
-        dispatchIngredientLists({
-          type: INGREDIENTS_ACTIONS.DELETE,
-          payload: { id }
-        });
-      } else {
-        throw new Error('Something went wrong while deleting ingredient.');
-      }
-    } catch (error) {
-      // setError(error.message);
-      dispatchHttpStatus({
-        type: HTTP_ACTIONS.ERROR,
-        payload: { error: error.message }
-      });
-    } finally {
-      // setIsLoading(false);
-      dispatchHttpStatus({ type: HTTP_ACTIONS.RESPONSE });
-    }
-  }, []);
+    //   if (response.ok && response.status === 200) {
+    //     // setIngredientLists((prevState) => prevState.filter(ingredient => ingredient.id !== id));
+    //     dispatchIngredientLists({
+    //       type: INGREDIENTS_ACTIONS.DELETE,
+    //       payload: { id }
+    //     });
+    //   } else {
+    //     throw new Error('Something went wrong while deleting ingredient.');
+    //   }
+    // } catch (error) {
+    //   // setError(error.message);
+    //   dispatchHttpStatus({
+    //     type: HTTP_ACTIONS.ERROR,
+    //     payload: { error: error.message }
+    //   });
+    // } finally {
+    //   // setIsLoading(false);
+    //   dispatchHttpStatus({ type: HTTP_ACTIONS.RESPONSE });
+    // }
 
-  const getIngredients = async () => {
-    try {
-      dispatchHttpStatus({ type: HTTP_ACTIONS.SEND });
-      const response = await fetch(URL);
+    sendRequest(``, 'DELETE', null, id, 'REMOVE_INGREDIENT');
+  }, [sendRequest]);
+
+  const getIngredients = useCallback(async () => {
+    // try {
+    //   dispatchHttpStatus({ type: HTTP_ACTIONS.SEND });
+    //   const response = await fetch(URL);
       
-      if (response.ok && response.status === 200) {
-        const data = await response.json();
+    //   if (response.ok && response.status === 200) {
+    //     const data = await response.json();
 
-        // setIngredientLists(Object.keys(data).map(key => ({
-        //   id: key,
-        //   title: data[key].title,
-        //   amount: data[key].amount
-        // })));
+    //     // setIngredientLists(Object.keys(data).map(key => ({
+    //     //   id: key,
+    //     //   title: data[key].title,
+    //     //   amount: data[key].amount
+    //     // })));
 
-        dispatchIngredientLists({
-          type: INGREDIENTS_ACTIONS.SET,
-          payload: {
-            ingredients: Object.keys(data).map(key => ({
-              id: key,
-              title: data[key].title,
-              amount: data[key].amount
-            }))
-          }
-        });
-      } else {
-        throw new Error('Something went wrong while fetching ingredients');
-      }
-    } catch (error) {
-      dispatchHttpStatus({
-        type: HTTP_ACTIONS.ERROR,
-        payload: { error: error.message }
-      });
-    } finally {
-      dispatchHttpStatus({ type: HTTP_ACTIONS.RESPONSE });
-    }
-  };
+    //     dispatchIngredientLists({
+    //       type: INGREDIENTS_ACTIONS.SET,
+    //       payload: {
+    //         ingredients: Object.keys(data).map(key => ({
+    //           id: key,
+    //           title: data[key].title,
+    //           amount: data[key].amount
+    //         }))
+    //       }
+    //     });
+    //   } else {
+    //     throw new Error('Something went wrong while fetching ingredients');
+    //   }
+    // } catch (error) {
+    //   dispatchHttpStatus({
+    //     type: HTTP_ACTIONS.ERROR,
+    //     payload: { error: error.message }
+    //   });
+    // } finally {
+    //   dispatchHttpStatus({ type: HTTP_ACTIONS.RESPONSE });
+    // }
+
+    sendRequest(URL, 'GET', null, null, 'GET_INGREDIENT');
+  }, [sendRequest]);
 
   const filterIngredients = useCallback(async (term) => {
-    try {
-      dispatchHttpStatus({ type: HTTP_ACTIONS.SEND });
-      const query = term.length ? `?orderBy="title"&equalTo="${term}"` : '';
-      const response = await fetch(`${URL}${query}`);
+    // try {
+    //   dispatchHttpStatus({ type: HTTP_ACTIONS.SEND });
+    //   const query = term.length ? `?orderBy="title"&equalTo="${term}"` : '';
+    //   const response = await fetch(`${URL}${query}`);
       
-      if (response.ok && response.status === 200) {
-        const data = await response.json();
+    //   if (response.ok && response.status === 200) {
+    //     const data = await response.json();
 
-        // setIngredientLists(Object.keys(data).map(key => ({
-        //   id: key,
-        //   title: data[key].title,
-        //   amount: data[key].amount
-        // })));
+    //     // setIngredientLists(Object.keys(data).map(key => ({
+    //     //   id: key,
+    //     //   title: data[key].title,
+    //     //   amount: data[key].amount
+    //     // })));
 
-        dispatchIngredientLists({
-          type: INGREDIENTS_ACTIONS.SET,
-          payload: {
-            ingredients: Object.keys(data).map(key => ({
-              id: key,
-              title: data[key].title,
-              amount: data[key].amount
-            }))
-          }
-        });
-      } else {
-        throw new Error('Something went wrong while searching ingredients');
-      }
-    } catch (error) {
-      dispatchHttpStatus({
-        type: HTTP_ACTIONS.ERROR,
-        payload: { error: error.message }
-      });
-    } finally {
-      dispatchHttpStatus({ type: HTTP_ACTIONS.RESPONSE });
-    }
-  }, []);
+    //     dispatchIngredientLists({
+    //       type: INGREDIENTS_ACTIONS.SET,
+    //       payload: {
+    //         ingredients: Object.keys(data).map(key => ({
+    //           id: key,
+    //           title: data[key].title,
+    //           amount: data[key].amount
+    //         }))
+    //       }
+    //     });
+    //   } else {
+    //     throw new Error('Something went wrong while searching ingredients');
+    //   }
+    // } catch (error) {
+    //   dispatchHttpStatus({
+    //     type: HTTP_ACTIONS.ERROR,
+    //     payload: { error: error.message }
+    //   });
+    // } finally {
+    //   dispatchHttpStatus({ type: HTTP_ACTIONS.RESPONSE });
+    // }
+
+    const query = term.length ? `?orderBy="title"&equalTo="${term}"` : '';
+
+    sendRequest(`${URL}${query}`, 'GET', null, null, 'FILTER_INGREDIENT');
+  }, [sendRequest]);
 
   const searchHandler = async (term) => {
     setSearchTerm(term);
@@ -217,8 +229,60 @@ function Ingredients() {
 
   const closeModalHandler = useCallback(() => {
     // setError(null);
-    dispatchHttpStatus({ type: HTTP_ACTIONS.CLEAR });
-  }, []);
+    // dispatchHttpStatus({ type: HTTP_ACTIONS.CLEAR });
+    clear();
+  }, [clear]);
+
+  useEffect(() => {
+    if (!isLoading && !error) {
+      switch (identifier) {
+        case 'GET_INGREDIENT':
+          dispatchIngredientLists({
+            type: INGREDIENTS_ACTIONS.SET,
+            payload: {
+              ingredients: Object.keys(data).map(key => ({
+                id: key,
+                title: data[key].title,
+                amount: data[key].amount
+              }))
+            }
+          });
+          break;
+
+        case 'ADD_INGREDIENT':
+          dispatchIngredientLists({
+            type: INGREDIENTS_ACTIONS.ADD,
+            payload: {
+              ingredient: { id: data.name, ...requestOption }
+            }
+          });
+          break;
+
+        case 'REMOVE_INGREDIENT':
+          dispatchIngredientLists({
+            type: INGREDIENTS_ACTIONS.DELETE,
+            payload: { id: requestOption }
+          });
+          break;
+
+        case 'FILTER_INGREDIENT':
+          dispatchIngredientLists({
+            type: INGREDIENTS_ACTIONS.SET,
+            payload: {
+              ingredients: Object.keys(data).map(key => ({
+                id: key,
+                title: data[key].title,
+                amount: data[key].amount
+              }))
+            }
+          });
+          break;
+      
+        default:
+          break;
+      }
+    }
+  }, [isLoading, error, data, requestOption, identifier]);
 
   /*
    * When an empty dependency array is passed as the second argument of the
@@ -227,8 +291,7 @@ function Ingredients() {
   */
   useEffect(() => {
     getIngredients();
-  }, []);
-
+  }, [getIngredients]);
 
   /*
    * When the array contains a dependency the `useEffect()` hook will act like
@@ -263,9 +326,9 @@ function Ingredients() {
 
   return (
     <div className="App">
-      { httpStatus.error ? <ErrorModal onClose={ closeModalHandler }>{ httpStatus.error }</ErrorModal> : null }
+      { error ? <ErrorModal onClose={ closeModalHandler }>{ error }</ErrorModal> : null }
 
-      <IngredientForm onAddIngredient={ addIngredientHandler } isLoading={ httpStatus.isLoading } />
+      <IngredientForm onAddIngredient={ addIngredientHandler } isLoading={ isLoading } />
 
       <section>
         <Search forwardedRef={ searchTermRef } onSearch={ searchHandler } />

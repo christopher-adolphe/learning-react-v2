81d072a6c856c0c2a42204b1c8f6e7dcd7e03d29
cadolphe <christopher.adolphe@gmail.com>
2024-04-02 01:24:24 +0400

    Update NewEvent component to display relevant UI based on the request sent

--- events-app/src/components/Events/NewEvent.jsx
+++ events-app/src/components/Events/NewEvent.jsx
@@ -3,6 +3,7 @@ import { useMutation } from '@tanstack/react-query';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 import { createNewEvent } from '../../util/http.js';
 
@@ -17,7 +18,7 @@ export default function NewEvent() {
   // `mutate` property which is a function we can invoke
   // any time we wish to make the `POST` request inside the
   // component's logic
-  const { mutate } = useMutation({
+  const { mutate, isPending, isError, error } = useMutation({
     mutationFn: createNewEvent,
   })
 
@@ -30,15 +31,23 @@ export default function NewEvent() {
   return (
     <Modal onClose={() => navigate('../')}>
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+        { isPending && 'Submitting...'}
+
+        { !isPending && (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+        ) }
       </EventForm>
+
+      { isError && <ErrorBlock
+        title="Failed to create event"
+        message={ error.info?.message || 'An error occurred while creating a new event. Please check your inputs.' } /> }
     </Modal>
   );
 }

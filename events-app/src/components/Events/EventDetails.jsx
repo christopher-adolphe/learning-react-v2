db6f9085df90f3766556a45bf79bddf8da2151c8
Christopher Adolphe <christopher.adolphe@gmail.com>
2024-04-02 15:44:18 +0400

    Set refetchType to none for invalidateQueries

--- events-app/src/components/Events/EventDetails.jsx
+++ events-app/src/components/Events/EventDetails.jsx
@@ -22,6 +22,7 @@ export default function EventDetails() {
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ['events'],
+        refetchType: 'none',
       });
 
       navigate('/events');

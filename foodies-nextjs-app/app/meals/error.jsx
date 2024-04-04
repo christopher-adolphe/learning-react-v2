38a07064aba358d5930a93c296836fba41e8d96e
Christopher Adolphe <christopher.adolphe@gmail.com>
2024-04-04 11:43:17 +0400

    Implement error handling for Meals page

---
+++ foodies-nextjs-app/app/meals/error.jsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function MealsError() {
+  return (
+    <main className="error">
+      <h1>Oops, something went wrong!</h1>
+
+      <p>Sorry, we were unable to fetch meals data.</p>
+    </main>
+  );
+}
--- foodies-nextjs-app/services/meals.js
+++ foodies-nextjs-app/services/meals.js
@@ -3,12 +3,14 @@ import sql from 'better-sqlite3';
 const MEAL_DB = sql('meals.db');
 
 async function delay() {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 }
 
 export async function fetchMeals() {
   // Setting an artificial delay to demonstrate loading states
   delay();
+
+  // throw new Error('Sorry could not establish connection to database!');
   
   return MEAL_DB.prepare('SELECT * FROM meals').all();
 }

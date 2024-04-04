479206d258cdd06bc95474b84fa080b649209f8f
Christopher Adolphe <christopher.adolphe@gmail.com>
2024-04-04 11:53:08 +0400

    Add NotFound component to handle access to non-existing routes

---
+++ foodies-nextjs-app/app/not-found.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not found</h1>
+
+      <p>It looks like you got lost...</p>
+      
+      <p>We can't find the page you are looking for.</p>
+
+      <p>
+        <Link href="/">Back To Home</Link>
+      </p>
+    </main>
+  );
+}
